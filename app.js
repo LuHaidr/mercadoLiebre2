42f40eebd6d427b2a86e8a4d57df2dc6bf4910e3
LuHaidr <lucia.haidr@gmail.com>
2022-06-21 20:06:59 -0300

    fixed express settings

--- app.js
+++ app.js
@@ -1,26 +1,29 @@
 const express = require('express');
+const path = require ("path")
 const app = express();
-app.use(express.static('public'));
+
+// app.use(express.static('public'));
+// app.set("puerto",process.env.PORT || 3001)
 
 const PORT = process.env.PORT || 4000
 const HOST = process.env.HOST || "localhost"
 
 app.get('/', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(path.resolve(__dirname + '/views/home.html');
 });
 
 app.get('/login', (req,res)=>{
-    res.sendFile(__dirname + '/views/login.html');
+    res.sendFile(path.resolve(__dirname + '/views/login.html');
 });
 
 app.get('/register', (req,res)=>{
-    res.sendFile(__dirname + '/views/register.html');
+    res.sendFile(path.resolve(__dirname + '/views/register.html');
 });
 
 app.get('/home', (req,res)=>{
-    res.sendFile(__dirname + '/views/home.html');
+    res.sendFile(path.resolve(__dirname + '/views/home.html');
 });
 
-app.listen(4000, ()=>{
+app.listen(app.get("puerto"), ()=>{
     console.log(`Server running at http://${HOST}:${PORT}/`);
 });
