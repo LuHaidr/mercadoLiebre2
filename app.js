35f92b3f702de53238f526770ad8b3e15f8a5ea0
LuHaidr <lucia.haidr@gmail.com>
2022-06-23 19:04:54 -0300

    link con CC fixed

--- app.js
+++ app.js
@@ -1,29 +1,25 @@
-const express = require('express');
-const path = require ("path")
-const app = express();
+const express = require('express')
+const path = require('path')
+const app = express()
 
-// app.use(express.static('public'));
+app.use(express.static('public'));
 // app.set("puerto",process.env.PORT || 3001)
 
 const PORT = process.env.PORT || 4000
-const HOST = process.env.HOST || "localhost"
+const HOST = process.env.HOST || 'localhost'
 
-app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname + '/views/home.html'));
-});
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname +'/views/home.html'))
+})
 
-app.get('/login', (req,res)=>{
-    res.sendFile(path.resolve(__dirname + '/views/login.html'));
-});
+app.get('/login', (req, res) => {
+  res.sendFile(path.resolve(__dirname +'/views/login.html'))
+})
 
-app.get('/register', (req,res)=>{
-    res.sendFile(path.resolve(__dirname + '/views/register.html'));
-});
+app.get('/register', (req, res) => {
+  res.sendFile(path.resolve(__dirname +'/views/register.html'))
+})
 
-app.get('/home', (req,res)=>{
-    res.sendFile(path.resolve(__dirname + '/views/home.html'));
-});
-
-app.listen(app.get("PORT"), ()=>{
-    console.log(`Server running at http://${HOST}:${PORT}/`);
-});
+app.listen(PORT, () => {
+  console.log(`Server running at http://${HOST}:${PORT}/`)
+})
